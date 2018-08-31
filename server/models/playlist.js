455e91b832c06dcb5c8a1d991ff43cd6faa62e00
k-cormack <k.cormack@gmail.com>
2018-08-31 16:19:24 -0600

    slowly building....

--- server/models/playlist.js
+++ server/models/playlist.js
@@ -2,13 +2,36 @@ let mongoose = require('mongoose')
 let Schema = mongoose.Schema
 let schemaName = 'Playlist'
 
+let songDetails = new Schema({
+    title: {
+      type: String,
+      // required: true
+    },
+    albumArt: {
+      type: Image,
+      // src: 'artworkUrl60.replace(/60x60/g, "250x250")'
+    },
+    artist: {
+        type: String,
+        //required: true
+    },
+    collection: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+    preview: {
+        type: URL
+    }
+})
 
 let schema = new Schema({
   name: {
     type: String,
     required: true
   },
-  songs: []
+  songs: [songDetails]
 })
 
 
